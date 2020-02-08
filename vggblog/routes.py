ca37b1f38eaef92b5f0ec805ce1c7dcd146007c4
Oluwakayode <fadojuk@gmail.com>
2020-02-09 00:32:40 +0100

    Set up registration form and performed user authentication

--- vggblog/routes.py
+++ vggblog/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, url_for, flash, redirect
-from vggblog import app
+from vggblog import app, db, bcrypt
 from vggblog.forms import RegistrationForm, LoginForm
 from vggblog.models import User, Post
+from flask_login import login_user
 
 
 posts = [
@@ -34,8 +35,12 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-      flash(f'Account created for {form.username.data}!', 'success')
-      return redirect(url_for('home'))
+      hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+      user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+      db.session.add(user)
+      db.session.commit()
+      flash('Your account has been created! You are now able to login', 'success')
+      return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
@@ -43,8 +48,9 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-      if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-        flash('You are logged in!', 'success')
+      user = User.query.filter_by(email=form.email.data).first()
+      if user and bcrypt.check_password_hash(user.password, form.password.data):
+        login_user(user, remember=form.remember.data)
         return redirect(url_for('home'))
       else:
         flash('Email or password incorrect', 'danger')
