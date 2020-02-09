2a49213392aa3987372a45e2551530693f7b9e8b
Oluwakayode <fadojuk@gmail.com>
2020-02-09 05:10:56 +0100

    deploy to heroku

--- vggblog/forms.py
+++ vggblog/forms.py
@@ -46,3 +46,7 @@ class UpdateAccountForm(FlaskForm):
       user = User.query.filter_by(email=email.data).first()
       if user:
         raise ValidationError('That email is taken. Please choose a different one.')
+
+
+# class PostForm(FlaskForm):
+
