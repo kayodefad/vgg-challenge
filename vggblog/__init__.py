30285fde545939c87d5e1485958216da60791c66
Oluwakayode <fadojuk@gmail.com>
2020-02-09 08:45:28 +0100

    made changes to navbar

--- vggblog/__init__.py
+++ vggblog/__init__.py
@@ -4,6 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['SECRET_KEY'] = '772e75c1a8946af71ac9964e2a262a4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
