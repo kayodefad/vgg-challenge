3e62c72700765310ed67f6bda49cdaa8bcd5b783
Oluwakayode <fadojuk@gmail.com>
2020-02-10 07:55:22 +0100

    Added a fetch post button

--- vggblog/routes.py
+++ vggblog/routes.py
@@ -31,7 +31,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
       hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-      user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+      user = User(username=form.username.data, password=hashed_password)
       db.session.add(user)
       db.session.commit()
       flash('Your account has been created! You are now able to login', 'success')
@@ -45,13 +45,13 @@ def login():
       return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-      user = User.query.filter_by(email=form.email.data).first()
+      user = User.query.filter_by(username=form.username.data).first()
       if user and bcrypt.check_password_hash(user.password, form.password.data):
         login_user(user, remember=form.remember.data)
         next_page = request.args.get('next')
         return redirect(next_page) if next_page else redirect (url_for('home'))
       else:
-        flash('Email or password incorrect', 'danger')
+        flash('Username or password incorrect', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
@@ -84,13 +84,11 @@ def account():
       picture_file = save_picture(form.picture.data)
       current_user.image_file = picture_file
     current_user.username = form.username.data
-    current_user.email = form.email.data
     db.session.commit()
     flash('Your account has been updated!', 'success')
     return redirect(url_for('account'))
   elif request.method == 'GET':
     form.username.data = current_user.username
-    form.email.data = current_user.email
   image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
   return render_template('account.html', title='Account', image_file=image_file, form=form)
 
